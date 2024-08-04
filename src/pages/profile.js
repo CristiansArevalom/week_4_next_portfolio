e5da9b7113381b630b707f51ffd899bf0b744ea3
Cristian arevalo <cristians.arevalom@gmail.com>
2024-08-04 12:27:48 -0500

    finished

---
+++ src/pages/profile.js
@@ -0,0 +1,99 @@
+
+export const skills = [
+  {
+    skill: "Java",
+    percentage: 90,
+  },
+  {
+    skill: "SQL",
+    percentage: 80,
+  },
+  {
+    skill: "Angular",
+    percentage: 60,
+  },
+  {
+    skill: "Javascrtipt",
+    percentage: 40,
+  },
+  {
+    skill: "Python",
+    percentage: 70,
+  },
+  {
+    skill: "React.js",
+    percentage: 100,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Senior Consultant at Nae",
+    description:
+      "Responsible for ensuring proper operation and mapping of datacenter services and connectivity in CMDB NITRO, as well as integration to ITSM Helix, validation of the service desk process (top desk) and resolution of tickets to support the operation.",
+    from: 2024,
+  },
+  {
+    title: "ITIL N1 Manager - Configuration Manager at Global Hitss",
+    description:
+      "Responsible for ensuring data integrity and keeping the CMDBs updated (UCMDB microfocus, IBM Control Desk, Claro's inhouse CMDB) through automation, control checks, configuration item status reviews, monitoring the process of transition to operation and lifecycle of the CI's focused on the Datacenter segment. Being an input for Claro's operation.",
+    from: 2020,
+    to: 2023,
+  },
+  {
+    title: "Configuration Manager Trainee at Claro",
+    description:
+      "Perform audits of the databases with the configuration elements, ensuring the reliability of the information, their relationships and network topologies. Manage and escalate to the technical areas the errors in the information contained in the control-desk CMDB. Support change leaders in verifying documentation and closing changes in the Change Management tool Update documentation and dissemination of use of the CMDB control-desk",
+    from: 2019,
+    to : 2020
+  },
+];
+
+export const projects = [
+  {
+    name: "API REST for Medical Appointments",
+    description:
+      "Basic api rest for Medical appointments using Java,Springboot, Mysql and JPA as ORM including the main data models like doctors, patients and medical appointment between doctors and patients.",
+    image: "PROJECT1.gif",
+  },
+  {
+    name: "School Enrollments ",
+    description:
+      "This project is a RESTful API for managing student enrollment records at an academy. It utilizes Java, Spring Boot, JPA as an ORM with PostgreSQL, authentication with JWT, and testing with JUnit and Mockito. The project is structured based on the MVC mode",
+    image: "project3.jfif",
+  },
+  {
+    name: "Full Stack Project for Medical Enviroment",
+    description:
+      "This project is a RESTful API for managing student enrollment records at an academy. It utilizes Java, Spring Boot, JPA as an ORM with PostgreSQL, authentication with JWT, and testing with JUnit and Mockito. The project is structured based on the MVC model.This project was developed for the program Java backend developer",
+    image: "Project2.gif",
+  },
+  {
+    name: "Movie Crud on Java",
+    description:
+      "Project MVC using java and Mysql",
+    image: "misionticmvc.gif",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
